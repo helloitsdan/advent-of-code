3c90efd5b6734a2f8169a4740b7f5d454005a4a9
Dani Sexton <dani.sexton@matillion.com>
2020-12-01 18:50:10 +0000

    day 1: happy accounting elves

---
+++ 1/accountingElves.ts
@@ -0,0 +1,42 @@
+import { DEFAULT_TARGET, POSSIBLE_NUMBERS } from "./haystack.ts";
+
+const getArrayProduct = (array: number[]) => array.reduce((a, b) => a * b);
+
+const findPair = (target = DEFAULT_TARGET, haystack = POSSIBLE_NUMBERS) => {
+  const splitAt = Math.round(haystack.length / 2);
+  const upper = haystack.slice(splitAt);
+  const lower = haystack.slice(0, splitAt);
+
+  let tuple: [number, number] | null = null;
+
+  for (const candidate of upper) {
+    const partner = target - candidate;
+    const hasPartner = lower.includes(partner);
+
+    if (hasPartner) {
+      tuple = [partner, candidate];
+      break;
+    }
+  }
+
+  return tuple ? ([...tuple, getArrayProduct(tuple)] as const) : null;
+};
+
+const findTriplet = (target = DEFAULT_TARGET, haystack = POSSIBLE_NUMBERS) => {
+  let tuple: [number, number, number] | null = null;
+
+  for (let idx = 0; idx < haystack.length; idx++) {
+    const candidate = haystack[idx];
+    const adjustedTarget = target - candidate;
+    const foundPair = findPair(adjustedTarget, haystack);
+
+    if (foundPair) {
+      tuple = [candidate, foundPair[0], foundPair[1]];
+      break;
+    }
+  }
+
+  return tuple ? [...tuple, getArrayProduct(tuple)] : null;
+};
+
+console.log(findPair(), findTriplet());
